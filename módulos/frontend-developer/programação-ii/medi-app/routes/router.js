d629dd87ecd7d39041572b7492868fb547cd736a
Roberta Fontes <70981960+betafontes@users.noreply.github.com>
2026-03-05 12:06:12 -0300

    feat(core): adição do middleware de autenticação na aplicação

--- módulos/frontend-developer/programação-ii/medi-app/routes/router.js
+++ módulos/frontend-developer/programação-ii/medi-app/routes/router.js
@@ -3,6 +3,9 @@ import appointmentController from './AppointmentController.js';
 import doctorController from './DoctorController.js';
 import pacientController from './PacientController.js';
 import prescriptionController from './PrescriptionController.js';
+import doctorService from '../services/DoctorService.js';
+import bcrypt from 'bcrypt';
+import verifyToken from './../middleware/authMiddleware';
 
 const router = express.Router();
 
@@ -10,9 +13,35 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello, World!' });
 });
 
-router.use('/', appointmentController);
-router.use('/', doctorController);
-router.use('/', pacientController);
-router.use('/', prescriptionController);
+// mapeamento do login
+router.post('/login', async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    const doctor = await doctorService.getDoctorByLogin(login);
+
+    if (!doctor) {
+      return res.status(401).json({ error: 'Invalid login!' });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, doctor.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Authentication failed!' });
+    }
+
+    const token = jwt.sign({doctorId: doctor._id}, 'you-secret-key', {
+      expiresIn: '1h'
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Login failed!' });
+  }
+});
+
+router.use('/', verifyToken, appointmentController);
+router.use('/', verifyToken, doctorController);
+router.use('/', verifyToken, pacientController);
+router.use('/', verifyToken, prescriptionController);
 
 export default router;
